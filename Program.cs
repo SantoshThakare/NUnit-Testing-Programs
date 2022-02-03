1724a5015035b2f2cd6b31592ed336b68f0ce33c
SantoshThakare <santoshthakare50@gmail.com>
2022-02-03 14:22:04 +0530

    [santosh]: Added Temperature Conversion

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NUnit_Testing;
+
+
+
+Console.WriteLine("Hello, World!");
+
+TemperatureConverter temperatures = new TemperatureConverter();
+temperatures.convert();
---
+++ TemperatureConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Testing
+{
+    public class TemperatureConverter
+    {
+        public double CelsiusToFahrenheit(string celsiusTemp)
+        {
+            double celsius = double .Parse(celsiusTemp);
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return fahrenheit;
+        }
+        public double FahrenheitToCelsius(string fahrenheitTemp)
+        {
+            double fahrenheit = double .Parse(fahrenheitTemp);
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+        public void convert()
+        {
+            
+            Console.WriteLine("Please provide celsius temperature");
+            string val1 = Console.ReadLine();
+            double fahrenheit = CelsiusToFahrenheit(val1);
+            Console.WriteLine($"value of {val1} celsius is {fahrenheit}fahrenheit");
+
+            Console.WriteLine("Please provide fahrenheit temperature");
+            string val2 = Console.ReadLine();
+            double celsius = FahrenheitToCelsius(val2);
+            Console.WriteLine($"value of {val2} fahrenheit is {celsius}celsius");
+
+        }
+            
+    }
+}
